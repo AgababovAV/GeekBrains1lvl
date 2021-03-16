0432d8bad67d1bb021603062a1cc48f398c491a9
avagababov <a.v.agababov@gmail.com>
2021-03-16 13:07:52 +0300

    first init;
    first init;

---
+++ WebStore/Startup.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebStore
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration Configuration)
+        {
+            this.Configuration = Configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env/*, IConfiguration config*/)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/Greetings", async context =>
+                {
+                    await context.Response.WriteAsync(Configuration["Greetings"]);
+                });
+
+                //endpoints.MapDefaultControllerRoute();
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
